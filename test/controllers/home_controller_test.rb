5661a316f2bb4d1e417dac1ff13d62665c9b280e
pfsamaja <pfsamaja@uc.cl>
2016-05-15 18:16:07 -0300

    1.0.3

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,5 +5,8 @@ class HomeControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
-
+  test "should render" do
+    post :buscar, tag: 'hola', access_token: '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'
+    assert_response :success
+   end
 end
