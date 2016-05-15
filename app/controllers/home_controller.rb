3d93ae0069ec1c1f38fb2efabac8f74b79bab8ba
pfsamaja <pfsamaja@uc.cl>
2016-05-15 14:54:49 -0300

    1.0.1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,17 @@
+#skip_before_action :verify_authenticity_token, if: :json_request?
+
 class HomeController < ApplicationController
+ skip_before_action :verify_authenticity_token
   def index
   end
   def buscar
-	params.require[:tag]
-	params.require[:access_token]
+	params.require(:tag)
+	params.require(:access_token)
 	
-	token = params[:tag]
-	tag = params[:access_token]
+	tag = params[:tag]
+	token = params[:access_token]
 	buffer = open("https://api.instagram.com/v1/tags/"+tag.to_s+"?access_token="+token.to_s).read
 	resultado = JSON.parse(buffer)
-	render resultado
+	render :json => resultado
   end
 end
